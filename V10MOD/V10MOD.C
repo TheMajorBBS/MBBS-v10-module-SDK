ce51f7902ed404adb08bafceb55ae9773c4aef23
Rick Hadsall <69971241+elwynor@users.noreply.github.com>
2025-02-24 17:14:19 -0500

    Modified LICENSE statement and linting

--- V10MOD/V10MOD.C
+++ V10MOD/V10MOD.C
@@ -9,10 +9,7 @@
  *                                                                           *
  *                            - Duckula / Questman 2/22/2025                 *
  *                                                                           *
- *   This example C file is free for you to use, however no rights to the    *
- *   compiled library files are assigned beyond the right to use them to     *
- *   build your modules. See the included LICENSE file for full licensing    *
- *   details                                                                 *
+ *   See LICENSE file in SDK root for terms and conditions.                  *
  *                                                                           *
  *****************************************************************************/
 
@@ -21,42 +18,42 @@
 #include "V10MOD.H"
 
 // function declarations
-GBOOL lonmod(VOID);                 /* Logon routine                        */
-GBOOL modrou(VOID);                 /* Main module routine                  */         
-VOID  delrou(CHAR* userid);         /* Delete account routine               */
-VOID storeUserRecord(CHAR* usr);    /* Store user record in DAT file        */
-VOID displayUserRecords(VOID);      /* Display users in DAT file            */
-VOID modcls(VOID);                  /* Shutdown routine                     */    
-VOID end_v10mod(VOID);              /* empty fcn to show EOF for debug      */
+GBOOL user_logon(VOID);              /* Logon routine                        */
+GBOOL module_main(VOID);             /* Main module routine                  */         
+VOID  user_deleted(CHAR* userid);    /* Delete account routine               */
+VOID  store_user_record(CHAR* userid); /* Store user record in DAT file      */
+VOID  display_user_records(VOID);    /* Display users in DAT file            */
+VOID  system_shutdown(VOID);         /* Shutdown routine                     */    
+VOID  end_v10mod(VOID);              /* empty fcn to show EOF for debug      */
 
 // module definition
-INT usrstt;                         /* User state in this module            */
-struct module V10MOD={              /* module interface block               */
-     "",                            /* name used to refer to this module    */
-     lonmod,                        /* user logon supplemental routine      */
-     modrou,                        /* input routine if selected            */
-     NULL,                          /* status-input routine if selected     */
-     NULL,                          /* "injoth" routine for this module     */
-     NULL,                          /* user logoff supplemental routine     */
-     NULL,                          /* hangup (lost carrier) routine        */
-     NULL,                          /* midnight cleanup routine             */
-     delrou,                        /* delete-account routine               */
-     modcls                         /* finish-up (sys shutdown) routine     */
+INT usrstt;                          /* User state in this module            */
+struct module V10MOD={               /* module interface block               */
+     "",                             /* name used to refer to this module    */
+     user_logon,                     /* user logon supplemental routine      */
+     module_main,                    /* input routine if selected            */
+     NULL,                           /* status-input routine if selected     */
+     NULL,                           /* "injoth" routine for this module     */
+     NULL,                           /* user logoff supplemental routine     */
+     NULL,                           /* hangup (lost carrier) routine        */
+     NULL,                           /* midnight cleanup routine             */
+     user_deleted,                   /* delete-account routine               */
+     system_shutdown                 /* finish-up (sys shutdown) routine     */
 };
 
 // global variables
-INT displom;                        /* Display logon message?               */
-HMCVFILE modmb;                     /* Module message file                  */
-DFAFILE* moddat;                    /* Module data file                     */
+GBOOL display_logon_msg;             /* Display logon message?               */
+HMCVFILE modmb;                      /* Module message file                  */
+DFAFILE* moddat;                     /* Module data file                     */
 
-struct moddat {                     /* Example module data file structure   */
-    CHAR name[UIDSIZ];              /* userid                               */            
+struct moddat {                      /* Example module data file structure   */
+    CHAR name[UIDSIZ];               /* userid                               */
 };
 
 // begin routines
 
 void EXPORT
-init__v10mod()                      /* initialize Module                    */
+init__v10mod(VOID)                   /* initialize Module                    */
 {
     // register the module
     stzcpy(V10MOD.descrp, gmdnam("V10MOD.MDF"), MNMSIZ);
@@ -64,7 +61,7 @@ init__v10mod()                      /* initialize Module                    */
 
     // open the message file
     modmb = opnmsg("V10MOD.MCV");
-    displom = ynopt(DISPLOM);
+    display_logon_msg = ynopt(DISPLOM);
 
     // open the data file
     moddat = dfaOpen("V10MOD.DAT", sizeof(struct moddat), NULL);
@@ -74,11 +71,10 @@ init__v10mod()                      /* initialize Module                    */
 }
 
 GBOOL EXPORT
-lonmod(VOID)                        /* User Logon Message                   */
+user_logon(VOID)                     /* User Logon Message                   */
 {
-    // If option to display logon message is true then output message
-    // to user
-    if (displom) {
+    // If option to display logon message is true then output message to user
+    if (display_logon_msg) {
         setmbk(modmb);
         prfmsg(LOGIN);
         outprf(usrnum);
@@ -89,38 +85,30 @@ lonmod(VOID)                        /* User Logon Message                   */
 }
 
 GBOOL EXPORT
-modrou(VOID)                        /* Main module routine                  */
+module_main(VOID)                   /* Main module input routine              */
 {
     setmbk(modmb);
     switch (usrptr->substt) {
     case 0:
         // Check if user exists in DAT file then display menu
-        storeUserRecord(usaptr->userid);
+        store_user_record(usaptr->userid);
         prfmsg(usrptr->substt=DISPMEN);
         break;
-    case DISPMEN:
-        // User selected to exit the module
+    case DISPMEN:        
         if (sameas(margv[0], "x")) {
-            prfmsg(LEAVE, usaptr->userid);
+            prfmsg(LEAVE, usaptr->userid); // User selected to exit the module
             return(0);
-        }
-        // Display short menu on no entry
-        else if (sameas(margv[0], "\0") || sameas(margv[0], "")) {
-            prfmsg(SHORTM);
-        }
-        // Re-display the menu on ?
+        }        
+        else if (sameas(margv[0], "\0") || sameas(margv[0], "")) 
+            prfmsg(SHORTM);                // Display short menu on no entry
         else if (sameas(margv[0], "?")) {
-            prfmsg(DISPMEN);
+            prfmsg(DISPMEN);               // Re-display the menu on ?
             break;
-        }
-        // Call routine to show users that are strored in V10MODULE.DAT
-        else if (sameas(margv[0], "d")) {
-            displayUserRecords();
-        }
-        // Invalid selection message
-        else {
-            prfmsg(INVSEL);
-        }
+        }        
+        else if (sameas(margv[0], "d")) 
+            display_user_records();        // list users stored in V10MODULE.DAT
+        else 
+            prfmsg(INVSEL);                // Invalid selection message
         prfmsg(SHORTM);
         break;
     }
@@ -130,23 +118,23 @@ modrou(VOID)                        /* Main module routine                  */
 }
 
 VOID EXPORT
-storeUserRecord(CHAR *usr)     /* Store user into DAT file */
+store_user_record(CHAR *userid)      /* Store user into DAT file              */
 {
     struct moddat modd;
 
     dfaSetBlk(moddat);
 
     // If user not already in DAT file, insert them
-    if (!dfaAcqEQ(&modd, usr, 0)) {
+    if (!dfaAcqEQ(&modd, userid, 0)) {
         setmem(&modd, sizeof(struct moddat), 0);
-        strcpy(modd.name, usr);
+        strcpy(modd.name, userid);
         dfaInsert(&modd);
     }
     dfaRstBlk();
 }
 
 VOID EXPORT 
-displayUserRecords(VOID)
+display_user_records(VOID)           /* List users from data file             */
 {
     struct moddat modd;
     dfaSetBlk(moddat);
@@ -156,41 +144,41 @@ displayUserRecords(VOID)
     // Read first record in DAT file
     recpos = dfaStepLO(&modd);
 
-    if (recpos == 1)
-    {
+    if (recpos == 1) {
         prfmsg(USRLSTS);
         prfmsg(USRLSTU, modd.name);
         while ((recpos = dfaStepNX(&modd)) != 0)
-        {
-            prfmsg(USRLSTU, modd.name);
-        };
+            prfmsg(USRLSTU, modd.name);        
         prfmsg(USRLSTE);
         outprf(usrnum);
-    }
-    else
-    {
+    } else {
         prfmsg(NOUSERS);
         outprf(usrnum);
     }
     dfaRstBlk();
 }
 
+/*
+ * If a user is deleted from the BBS, 
+ * delete them from the module's database.
+ */
 VOID EXPORT
-delrou(CHAR* userid)                /* if a user is deleted from the BBS, delete them if they're in our file */
+user_deleted(CHAR* userid)       
 {
     dfaSetBlk(moddat);
-    if (dfaAcqEQ(NULL, userid, 0)) dfaDelete();
+    if (dfaAcqEQ(NULL, userid, 0)) 
+        dfaDelete();
     dfaRstBlk();
 }
 
 VOID EXPORT
-modcls(VOID)                        /* close MSG and DAT files for shutdown */
+system_shutdown(VOID)                /* close MSG and DAT files for shutdown  */
 {
     clsmsg(modmb);
     dfaClose(moddat);
 }
 
 VOID EXPORT
-end_v10mod(VOID)
+end_v10mod(VOID)                     /* used to help GALEXCEP.OUT analysis    */
 {
 }
